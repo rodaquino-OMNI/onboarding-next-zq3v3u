601734d288a33377c56c4bb2a77ee8f5f67f2e95
rodaquino-OMNI <rodrigotaquino@gmail.com>
2025-02-06 14:32:45 -0300

    Add public-samples/onboarding-next-zq3v3u/src/backend/lang/pt-BR/pagination.php

---
+++ src/backend/lang/pt-BR/pagination.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Laravel pagination language lines for Brazilian Portuguese (pt-BR)
+ * 
+ * Package: illuminate/translation ^9.0
+ * Encoding: UTF-8
+ * 
+ * These translations provide culturally appropriate navigation text for pagination controls
+ * following Brazilian Portuguese language conventions and accessibility standards.
+ * 
+ * @see https://laravel.com/docs/9.x/pagination
+ */
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Pagination Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    'previous' => '« Anterior',
+    'next'     => 'Próximo »',
+
+];
